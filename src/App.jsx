7afa3958f84bb64c131b63b53d98e27bdce3f2a1
Devveloper122 <158509966+Devveloper122@users.noreply.github.com>
2024-05-09 17:46:14 +0530

    react hook challenge -7

--- src/App.jsx
+++ src/App.jsx
@@ -3,19 +3,17 @@ import { useEffect } from "react";
 
 const App =()=>{
     const [score ,Setscore] = useState(0);
-    const [score2 ,Setscore2] = useState(0);
     
     // this 'useEffect' will automatically be called after render 
     // method is called.
     useEffect(()=>{
-        alert('I am Clicked');
+        // alert('I am Clicked');
+        document.title = `you clicked me ${score} times.`;
     } ,[score]);
 
     return(
         <>
             <button onClick={()=>{Setscore(score+1)}}> Click me {score} </button>;
-            <br/>;
-            <button onClick={()=>{Setscore2(score2+1)}}> Click me {score2} </button>;
         </>
     );
 
