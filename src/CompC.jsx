c60fe8e42d7ab008d84c0468647b0458ef03d864
Devveloper122 <158509966+Devveloper122@users.noreply.github.com>
2024-05-09 17:02:28 +0530

    useContext to make context easier

--- src/CompC.jsx
+++ src/CompC.jsx
@@ -1,23 +1,15 @@
-import React from "react";
+import React, { useContext } from "react";
 import { FirstName ,LastName} from "./App";
 
 const CompC=()=>{
+    // always use react hooks inside and at the top of Component 
+    // function.
+    const fname = useContext(FirstName);
+    const lname = useContext(LastName);
+
     return (
-        // <h1>My name is Akhil Singh</h1>
         <>
-            <FirstName.Consumer> 
-                {(fname)=>{
-                    return (
-                        <LastName.Consumer>
-                            {(lname)=>{
-                                return (
-                                    <h1> My name is {fname} {lname} </h1>
-                                );
-                            }}
-                        </LastName.Consumer>
-                    );
-                }}
-            </FirstName.Consumer>
+            <h1>My name is {fname} {lname} </h1>
         </>
     );
 };
