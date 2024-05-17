269fcce00ebcc1f2b1160ccba9fcb9e6e398b702
Devveloper122 <158509966+Devveloper122@users.noreply.github.com>
2024-05-17 16:09:17 +0530

    how to use API

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import CompA from './CompA';
 
 import './index.css';
 
-ReactDOM.render( <App/>,
+ReactDOM.render( <CompA/>,
     document.getElementById('root') 
 );
