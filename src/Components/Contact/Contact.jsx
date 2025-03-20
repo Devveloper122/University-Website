e66b26f1982c7f2f878141ec967795d332631b3d
Devveloper122 <158509966+Devveloper122@users.noreply.github.com>
2025-03-20 20:43:37 +0530

    Update Contact.jsx

--- src/Components/Contact/Contact.jsx
+++ src/Components/Contact/Contact.jsx
@@ -47,7 +47,7 @@ const Contact = () => {
 
         <div className="contact-col">
             <form onSubmit={onSubmit}>
-                <label >Your name</label>
+                <label >Your Name</label>
                 <input type="text" name='name' placeholder='Enter your name' required />
                 <label >Phone number</label>
                 <input type="tell" name='phone' placeholder='Enter your mobile number' required/>
